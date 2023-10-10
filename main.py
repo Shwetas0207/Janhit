51ca4f2d4a9471cb8d90bc6114e925f100944004
Shwetas0207 <134207070+Shwetas0207@users.noreply.github.com>
2023-10-10 23:59:35 +0530

    lkiuytfghgghjk

--- main.py
+++ main.py
@@ -15,13 +15,22 @@ origins = [
     "http://127.0.0.1:5500",  # Add the origin of your HTML file here
 ]
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Running the app with HTTPS support via Uvicorn
 if __name__ == "__main__":
--- route/user.py
+++ route/user.py
@@ -3,54 +3,60 @@ from fastapi.responses import JSONResponse
 from model.user import *
 from Core.db import *
 from fastapi import FastAPI, APIRouter
+from fastapi import HTTPException, requests
+import json, pydantic
 
 
 
 router = APIRouter()
 
-
 @router.post("/contact", response_model=dict)
-async def contact_form(json_data: dict):  
-    contact = Contact(**json_data)
-    a = contactdb.find_one({"Email": contact.email})  
-    if a:
-        return JSONResponse({'error': 'Same email'}, status_code=400)
-    data = {'Name': contact.name,  # Use contact.name, contact.email, etc.
-            'Email': contact.email,
-            'Phone': contact.phone,
-            'Message': contact.message}
-    contactdb.insert_one(data)
-    return JSONResponse({'mess': 'Contact Saved'}, status_code=200)
+async def contact_form(contact: Contact):  
+    # Check if an entry with the same email exists in the database
+    existing_contact = contactdb.find_one({"Email": contact.email})  
+    if existing_contact:
+        return JSONResponse({'error': 'Same email'}, status_code=400)   
+    # Create a dictionary to insert into the database
+    data = {
+        'Name': contact.name,
+        'Email': contact.email,
+        'Phone': contact.phone,
+        'Message': contact.message
+    }   
+    # Insert the data into the database
+    contactdb.insert_one(data)    
+    return JSONResponse({'message': 'Contact Saved'}, status_code=200)
 
 
-
-
-
-@router.post('/join')
-async def join(mod:Join):
-    a=['male','female','others']
-    if mod.gender not in  a:
-        return JSONResponse({'error':'Select form the list'},status_code=400)
-    data={
-        'Fname':mod.fname,
-        'Lname':mod.lname,
-        'Gender':mod.gender,
-        'About':mod.about}
-    joindb.insert_one(data)
-    return JSONResponse({'mess':'Success'},status_code=200)
+@router.post("/user/join", response_model=dict)
+async def join_user(join: Join):
+    try:
+        allowed_genders = ['male', 'female', 'other']
+        if join.gender.lower() not in allowed_genders:
+            return JSONResponse({'error': 'Invalid gender'}, status_code=400)
+        join_data = {
+            'First Name': join.fname,
+            'Last Name': join.lname,
+            'Gender': join.gender,
+            'About': join.about
+        }
+        joindb.insert_one(join_data)
+        # Here, we're just returning the data as a response
+        return JSONResponse({'message': 'Join request submitted successfully', 'data': join_data}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
 
+@router.post("/donation", response_model=dict)
+async def donation_form(donation: Donation):  
+    if donation.amount <= 0:
+        return JSONResponse({'error': 'Same email'}, status_code=400)    
+    data = {
+        'Name': donation.name,
+        'Email': donation.email,
+        'Phone': donation.phone,
+        'Amount': donation.amount
+    }    
+    donationdb.insert_one(data)    
+    return JSONResponse({'message': 'Contact Saved'}, status_code=200)
 
-
-@router.post('/donation')
-async def donation(mod:Donation):
-    if mod.amount<0:
-        return JSONResponse({'error':'Enter number greater than 0'},status_code=400)
-    data={
-        'Name':mod.name,
-        'Email':mod.email,
-        'Phone':mod.phone,
-        'Amount':mod.amount
-    }   
-    donationdb.insert_one(data)
-    return JSONResponse({'mess':'Success '},status_code=200)
