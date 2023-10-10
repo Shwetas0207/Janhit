2ae4006439ba305eea4e61fd05769b6160221321
Rhul27 <66371376+Rhul27@users.noreply.github.com>
2023-10-11 02:11:44 +0530

    cxvcx

--- main.py
+++ main.py
@@ -15,17 +15,10 @@ origins = [
     "http://127.0.0.1:5500",  # Add the origin of your HTML file here
 ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
